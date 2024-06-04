f7b1f8868c93be5ec4438ace1777ba115b48ecb6
김태근 <oryukdo3@pusan.ac.kr>
2024-06-04 14:12:53 +0900

    feat: 기름에는 0과 9사이의 숫자가 아닌 값을 입력하면 예외가 발생한다.(prod, test)

--- racingcar/src/main/java/Fuel.java
+++ racingcar/src/main/java/Fuel.java
@@ -5,10 +5,17 @@ public class Fuel {
     private int status;
 
     public Fuel(int status) {
+        fuelValidation(status);
         this.status = status;
     }
 
     public boolean isAbleToUse() {
         return this.status >= GOOD_FUEL_STD;
+    }
+
+    private static void fuelValidation(int status) {
+        if (status > 9 || status < 0){
+            throw new IllegalArgumentException("기름은 0과 9사이의 숫자만 입력이 가능합니다.");
+        }
     }
 }
--- racingcar/src/test/java/CarTest.java
+++ racingcar/src/test/java/CarTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -12,6 +14,13 @@ public class CarTest {
         int carPosition = car.forward();
 
         assertThat(carPosition).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 10})
+    @DisplayName("기름에는 0과 9사이의 숫자가 아닌 값을 입력하면 예외가 발생한다.")
+    void fuel_ShouldNot0UnderAnd9Over(int input){
+        assertThatThrownBy(() -> new Fuel(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
