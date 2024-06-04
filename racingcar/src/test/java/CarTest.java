e9213584b5d1f6abc9d46e347f30ab7b22fb66cd
김태근 <oryukdo3@pusan.ac.kr>
2024-06-04 14:14:30 +0900

    refactor: 기름 객체 테스트코드 분리

--- racingcar/src/test/java/CarTest.java
+++ racingcar/src/test/java/CarTest.java
@@ -15,24 +15,4 @@ public class CarTest {
 
         assertThat(carPosition).isEqualTo(2);
     }
-
-    @ParameterizedTest
-    @ValueSource(ints = {-1, 10})
-    @DisplayName("기름에는 0과 9사이의 숫자가 아닌 값을 입력하면 예외가 발생한다.")
-    void fuel_ShouldNot0UnderAnd9Over(int input){
-        assertThatThrownBy(() -> new Fuel(input)).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("4이상일 경우 좋은 기름이 생성된다.")
-    void goodFuel_ShouldOverFour(){
-        Fuel goodFuel = new Fuel(5);
-        Fuel badFuel = new Fuel(1);
-        boolean goodFuelResult = goodFuel.isAbleToUse();
-        boolean badFuelResult = badFuel.isAbleToUse();
-
-        assertThat(goodFuelResult).isTrue();
-        assertThat(badFuelResult).isFalse();
-
-    }
 }
