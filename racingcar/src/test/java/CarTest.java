cde71db58c4f819dbfdde739fe5fcc2cf81173b3
김태근 <oryukdo3@pusan.ac.kr>
2024-06-04 15:20:22 +0900

    test: 자동차는 안 좋은 기름을 넣으면 전진하지 않는다.

--- racingcar/src/test/java/CarTest.java
+++ racingcar/src/test/java/CarTest.java
@@ -7,12 +7,23 @@ public class CarTest {
 
     @Test
     @DisplayName("자동차 좋은 기름을 넣으면 1칸 전진한다.")
-    void car_ShouldForward(){
+    void car_ShouldMove1(){
 
         Car car = new Car("myCar1", 1);
         Fuel fuel = new Fuel(5);
 
         int carPosition = car.move(fuel);
         assertThat(carPosition).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("자동차 안 좋은 기름을 넣으면 전진하지 않는다.")
+    void car_ShouldNotMove(){
+
+        Car car = new Car("myCar1", 1);
+        Fuel fuel = new Fuel(1);
+
+        int carPosition = car.move(fuel);
+        assertThat(carPosition).isEqualTo(1);
     }
 }
